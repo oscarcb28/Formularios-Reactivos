6259d2caa5660ca4f5003d99e795a5d16a6a8252
oscar <oscarcapinbouzan@gmail.com>
2025-04-09 16:39:56 +0200

    1

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Form, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRadioModule} from '@angular/material/radio';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule, CommonModule, MatButtonModule, MatRadioModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+get nick(){
+  return this.formUser.get('nick') as FormControl;
+}
+
+get password(){
+  return this.formUser.get('password') as FormControl;
+}
+
+get email(){
+  return this.formUser.get('email') as FormControl;
+}
+
+get gender(){
+  return this.formUser.get('gender') as FormControl;
+}
+
+get description(){
+  return this.formUser.get('description') as FormControl;
+}
+
+formUser = new FormGroup({
+  'nick' : new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+  'password' : new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]),
+  'email' : new FormControl('', [Validators.required, Validators.email]),
+  'gender' : new FormControl('', Validators.required),
+  'description' : new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(100)])
+});
+
+  
+// nick = new FormControl('', Validators.required);
+// password = new FormControl('', Validators.required);
+// email = new FormControl('', [Validators.required, Validators.email]);
+// gender = new FormControl('', Validators.required);
+// description = new FormControl('', Validators.required);
+
+send(){
+  console.log(this.formUser.value);
+}
+
+
+}
